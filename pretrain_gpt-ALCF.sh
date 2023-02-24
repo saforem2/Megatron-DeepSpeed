ad3afafbc525d5c9ae18c55889d90102e80d3fa1
Sam Foreman <saforem2@gmail.com>
2023-02-24 10:32:10 -0600

    Updates `pretrain_gpt-ALCF.sh`, removes redundant file

--- pretrain_gpt-ALCF.sh
+++ pretrain_gpt-ALCF.sh
@@ -3,12 +3,6 @@
 TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -LP)
 
-echo "Job started at: ${TSTAMP} on $(hostname)"
-echo "Job running in: ${DIR}"
-
-export NCCL_DEBUG=warn
-export WANDB_CACHE_DIR="./cache/wandb"
-
 # ┏━━━━━━━━━━┓
 # ┃ ThetaGPU ┃
 # ┗━━━━━━━━━━┛
@@ -16,15 +10,12 @@ setupThetaGPU() {
   if [[ $(hostname) == theta* ]]; then
     export MACHINE="ThetaGPU"
     HOSTFILE="${COBALT_NODEFILE}"
-
     # -- Python / Conda setup -------------------------------------------------
     module load conda/2022-07-01 ; conda activate base
     conda activate \
       /lus/grand/projects/datascience/foremans/locations/thetaGPU/miniconda3/envs/2022-07-01
     source ./venvs/thetaGPU/2022-07-01-deepspeed/bin/activate
-    # -------------------------------------------------------------------------
-
-    # -- MPI / Comms Setup ----------------------------------
+    # -- MPI / Comms Setup ----------------------------------------------------
     NRANKS=$(wc -l < "${HOSTFILE}")
     NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
     NGPUS=$((${NRANKS}*${NGPU_PER_RANK}))
@@ -41,7 +32,6 @@ setupThetaGPU() {
     MPI_ELASTIC="\
       -n ${NGPUS} \
       -npernode ${NGPU_PER_RANK}"
-    # -------------------------------------------------------
   else
     echo "Unexpected hostname: $(hostname)"
   fi
@@ -54,18 +44,17 @@ setupPolaris()  {
   if [[ $(hostname) == x* ]]; then
     export MACHINE="Polaris"
     HOSTFILE="${PBS_NODEFILE}"
+    # -- Python / Conda setup -------------------------------------------------
     module load conda/2023-01-10
     conda activate base
     conda activate \
       /lus/grand/projects/datascience/foremans/locations/polaris/miniconda3/envs/2023-01-10
-
+    # -- MPI / Comms Setup ----------------------------------------------------
     # export IBV_FORK_SAFE=1
-    # -----------------------------------------------
     NRANKS=$(wc -l < "${HOSTFILE}")
     NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
     NGPUS=$((${NRANKS}*${NGPU_PER_RANK}))
     MPI_COMMAND=$(which mpiexec)
-    # -----------------------------------------------
     MPI_DEFAULTS="\
       --envall \
       --verbose \
@@ -78,47 +67,91 @@ setupPolaris()  {
   fi
 }
 
-
+export NCCL_DEBUG=warn
+export WANDB_CACHE_DIR="./cache/wandb"
 export CFLAGS="-I${CONDA_PREFIX}/include/"
 export LDFLAGS="-L${CONDA_PREFIX}/lib/"
 
-TP=8
-PP=16
-NLAYERS=24
-HIDDEN=1024
-GLOBAL_BATCH=64
-MICRO_BATCH=4
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Model / Architecture settings                       ┃
+# ┃ --------------------------------------------------- ┃
+# ┃ GPT-3 models use 2K sequence length/context window  ┃
+# ┃ The "GPT-3 XXX" below are configs from GPT-3 paper  ┃
+# ┃ https://arxiv.org/abs/2005.14165, choose based on   ┃
+# ┃ your desired model size or build your own configs   ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+SEQ_LEN=4096
+
+# ┏━━━━━━━━━━━━━━━━━━━━┓
+# ┃ GPT-3: 2.7B Params ┃
+# ┗━━━━━━━━━━━━━━━━━━━━┛
+# MODEL_SIZE="2.7B"
+# NLAYERS=32
+# HIDDEN=2560
+# ATEN_HEADS=32
+# GLOBAL_BATCH=512
+
+# ┏━━━━━━━━━━━━━━━━━━━━┓
+# ┃ GPT-3: 6.7B Params ┃
+# ┗━━━━━━━━━━━━━━━━━━━━┛
+MODEL_SIZE="6.7B"
+NLAYERS=32
+HIDDEN=4096
+ATEN_HEADS=32
+GLOBAL_BATCH=1024
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Model Parallel / Pipeline Parallel ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+MPSIZE=8
+PPSIZE=16
+MICRO_BATCH=1
 ZERO_STAGE=1
-RUN_STR=ds_z${ZERO_STAGE}_nl${NLAYERS}_hs${HIDDEN}_gb${GLOBAL_BATCH}_mb${MICRO_BATCH}
+
+# ┏━━━━━━━━━━━━┓
+# ┃ Data paths ┃
+# ┗━━━━━━━━━━━━┛
 DATA_PATH="./dataset/BookCorpusDataset_text_document"
 VOCAB_FILE="./dataset/gpt2-vocab.json"
 MERGE_FILE="./dataset/gpt2-merges.txt"
-OUTPUT_DIR="outputs/${RUN_STR}"
+
+# ┏━━━━━━━━━━━━━━━━━━━┓
+# ┃ FILE I/O SETTINGS ┃
+# ┗━━━━━━━━━━━━━━━━━━━┛
+RUN_STR="gb${GLOBAL_BATCH}_mb${MICRO_BATCH}"
+RUN_STR="nl${NLAYERS}_hs${HIDDEN}_${RUN_STR}"
+RUN_STR="mp${MPSIZE}_pp${PPSIZE}_${RUN_STR}"
+RUN_STR="z${ZERO_STAGE}_seqlen${SEQ_LEN}_${RUN_STR}"
+RUN_STR="GPT3_${MODEL_SIZE}_${RUN_STR}"
+OUTPUT_DIR="./outputs/${RUN_STR}"
+OUTPUT_LOG="./outputs/${RUN_STR}/logs/$TSTAMP.log"
 CHECKPOINT_DIR="./checkpoints/$RUN_STR"
 TENSORBOARD_DIR="./outputs/${RUN_STR}/tensorboard"
 export TENSORBOARD_DIR=$TENSORBOARD_DIR
 mkdir -p $OUTPUT_DIR/tensorboard/wandb
 mkdir -p $CHECKPOINT_DIR
 mkdir -p $TENSORBOARD_DIR
+mkdir -p "$(dirname "${OUTPUT_LOG}")"
+echo "${OUTPUT_LOG}" >> logfiles
 
+# ┏━━━━━━━━━━━━━━━━━━┓
+# ┃ DeepSpeed Config ┃
+# ┗━━━━━━━━━━━━━━━━━━┛
 DS_CONFIG=./ds_config-gpt.json
 cat <<EOT > $DS_CONFIG
 {
   "train_batch_size" : $GLOBAL_BATCH,
   "train_micro_batch_size_per_gpu": $MICRO_BATCH,
   "steps_per_print": 1,
-
   "zero_optimization": {
     "stage": $ZERO_STAGE
   },
-
   "fp16": {
     "enabled": true,
     "initial_scale_power": 12
   },
-
   "wall_clock_breakdown" : true,
-
   "flops_profiler": {
     "enabled": true,
     "profile_step": -1,
@@ -127,43 +160,37 @@ cat <<EOT > $DS_CONFIG
     "detailed": true,
     "output_file": null
   },
-
   "wandb": {
     "enabled": true,
     "project": "megatron-LM"
   }
-
 }
 EOT
 
-# "csv_monitor": {
-#   "enabled": true,
-#   "output_path": "$OUTPUT_DIR/ds_logs/",
-#   "job_name": "pretrain-gpt",
-#   "group": "polaris"
-# }
-
+# ┏━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ DeepSpeed Arguments ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━┛
 ds_args=""
 ds_args=" --deepspeed ${ds_args}"
 ds_args=" --deepspeed_mpi ${ds_args}"
 ds_args=" --deepspeed_config=$DS_CONFIG ${ds_args}"
 ds_args=" --zero-stage=$ZERO_STAGE ${ds_args}"
 ds_args=" --deepspeed-activation-checkpointing ${ds_args}"
-# ds_args=" --no-pipeline-parallel ${ds_args}"
 
-  # --save $CHECKPOINT_DIR \
-  # --load $CHECKPOINT_DIR \
+# ┏━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ MEGATRON-LM SETTINGS ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━┛
 gpt_args="\
-  --tensor-model-parallel-size $TP \
-  --pipeline-model-parallel-size $PP \
+  --tensor-model-parallel-size $MPSIZE \
+  --pipeline-model-parallel-size $PPSIZE \
   --num-layers $NLAYERS \
   --hidden-size $HIDDEN \
-  --num-attention-heads 16 \
-  --micro-batch-size 4 \
-  --global-batch-size 16 \
-  --seq-length 1024 \
-  --max-position-embeddings 1024 \
-  --train-iters 200 \
+  --num-attention-heads ${ATEN_HEADS} \
+  --micro-batch-size ${MICRO_BATCH} \
+  --global-batch-size ${GLOBAL_BATCH} \
+  --seq-length ${SEQ_LEN} \
+  --max-position-embeddings ${SEQ_LEN} \
+  --train-iters 10 \
   --lr-decay-iters 320000 \
   --data-path $DATA_PATH \
   --vocab-file $VOCAB_FILE \
@@ -180,14 +207,16 @@ gpt_args="\
   --checkpoint-activations \
   --log-interval 1 \
   --save-interval 1000 \
-  --eval-interval 50 \
-  --eval-iters 100 \
+  --eval-interval 5 \
+  --eval-iters 10 \
   --tensorboard-dir ${TENSORBOARD_DIR} \
   --log-timers-to-tensorboard \
-  --tensorboard-log-interval 5 \
+  --tensorboard-log-interval 1 \
   --fp16"
 
-
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ SETUP CONDA + MPI ENVIRONMENT @ ALCF ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 setup() {
   if [[ $(hostname) == theta* ]]; then
     echo "Setting up ThetaGPU from $(hostname)"
@@ -198,7 +227,6 @@ setup() {
   else
     echo "Unexpected hostname $(hostname)"
   fi
-
   export NODE_RANK=0
   export NNODES=$NRANKS
   export GPUS_PER_NODE=$NGPU_PER_RANK
@@ -219,6 +247,9 @@ singleGPU() {
   ${EXEC} "$@"
 }
 
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Use all available GPUs a single nodes ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 fullNode() {
   NRANKS=1
   NGPU_PER_RANK=$(nvidia-smi -L | wc -l)
@@ -235,7 +266,6 @@ fullNode() {
     ./pretrain_gpt.py \
     ${gpt_args} \
     ${ds_args}"
-
   echo "EXEC: $EXEC" "$@"
   ${EXEC} "$@"
 }
@@ -262,6 +292,16 @@ elasticDistributed() {
   ${EXEC} "$@"
 }
 
+printJobInfo() {
+  echo "Job started at: ${TSTAMP} on $(hostname)" 
+  echo "Job running in: ${DIR}" 
+  echo "Training GPT-3 with ${MODEL_SIZE} parameters" 
+  echo "Writing logs to: ${OUTPUT_LOG}" 
+  echo "to view output: 'tail -f $(tail -1 logfiles)'"
+}
+
 setup
-# singleGPU "$@"
-elasticDistributed "$@"
+printJobInfo | tee -a ${OUTPUT_LOG}
+# singleGPU "$@" >> ${OUTPUT_LOG} 2>&1 &
+# fullNode  "$@" >> ${OUTPUT_LOG} 2>&1 &
+elasticDistributed "$@"  >> ${OUTPUT_LOG} 2>&1 &
