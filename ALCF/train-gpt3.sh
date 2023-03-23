a54bbe716935704a664c0d82afa91b12e7bf0d02
Sam Foreman <saforem2@gmail.com>
2023-03-23 13:26:53 -0500

    Update `ALCF/train-gpt3.sh`

--- ALCF/train-gpt3.sh
+++ ALCF/train-gpt3.sh
@@ -3,7 +3,10 @@
 TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -LP)
 # PARENT=$(dirname "${DIR}")
-#
+
+#┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+#┃ Make sure we're not already running; if so, exit here ┃
+#┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 PIDS=$(ps aux | grep pretrain_gpt.py | grep -v grep | awk '{print $2}')
 if [ -n "${PIDS}" ]; then
   echo "Already running! Exiting!"
@@ -11,6 +14,10 @@ if [ -n "${PIDS}" ]; then
 fi
 
 
+#┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+#┃ source ./launch.sh                       ┃
+#┃ which then sources ./{args.sh,setup.sh}  ┃
+#┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 LAUNCH_FILE="${DIR}/launch.sh"
 if [[ -f "${LAUNCH_FILE}" ]]; then
   echo "source-ing ${LAUNCH_FILE}"
@@ -19,10 +26,9 @@ if [[ -f "${LAUNCH_FILE}" ]]; then
 else
   echo "ERROR: UNABLE TO SOURCE ${LAUNCH_FILE}"
 fi
-# source "${DIR}/launch.sh"
 
 
 setup
 # singleGPU "$@" 2>&1 &
-fullNode "$@" 2>&1 &
-# elasticDistributed "$@" 2>&1 &
+# fullNode "$@" 2>&1 &
+elasticDistributed "$@" 2>&1 &
