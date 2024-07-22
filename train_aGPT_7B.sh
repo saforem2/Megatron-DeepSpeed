5d96d64f96be27b62cf4a10d8440aeed9694b7ab
Sam Foreman <saforem2@gmail.com>
2024-07-22 08:33:52 -0500

    Update `train_aGPT_7B.sh`

--- train_aGPT_7B.sh
+++ train_aGPT_7B.sh
@@ -7,6 +7,7 @@
 # AuroraGPT-7B @ ALCF
 #####################################
 
+
 # 1. Navigate into `$PBS_O_WORKDIR`
 cd "${PBS_O_WORKDIR}" || exit
 HERE=$(python3 -c 'import os; print(os.getcwd())') && export HERE
@@ -14,14 +15,10 @@ HERE=$(python3 -c 'import os; print(os.getcwd())') && export HERE
 source "${HERE}/ALCF/helpers.sh" || exit
 # 3. call `setup` from `./ALCF/helpers.sh`
 setup "$@" || exit
-# # 4. Take custom args
-# export custom_args=" $@"
-# 5. Update ${run_cmd} (from setup ALCF/helpers.sh) with ${custom_args}
 export run_cmd="${run_cmd}"
-# 6. Add "${run_cmd}" to output log
 echo "${run_cmd}" | tee -a "${OUTPUT_LOG}"
 # 7. Tell user where to find output
 printf "[!! %s] View output at:\n %s\n" "$(printBlue "NOTE")" "$(printYellow "${OUTPUT_LOG}")" | tee -a "${OUTPUT_LOG}"
+XPU_IGNORE_STRING="CCL_WARN|\ -\ INFO\ \-\ |real_accelerator\.py|numexpr\.utils|async_io|libaio"
 # 8. Evaluate ${run_cmd} and append outputs to ${OUTPUT_LOG}
-eval "${run_cmd}" |& tee -a "${OUTPUT_LOG}"
-set +x
+eval "${run_cmd}" |& grep -E -v "${XPU_IGNORE_STRING}" |& tee -a "${OUTPUT_LOG}"
